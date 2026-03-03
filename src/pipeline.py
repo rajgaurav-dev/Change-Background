e8fbb68c163d84cd6b0942823724d91679764e70
Raj Gaurav <rgt.smarty@gmail.com>
2026-03-03 13:56:04 +0530

    feat:Added script for the pipeline.py

---
+++ src/pipeline.py
@@ -0,0 +1,17 @@
+import torch
+from diffusers import StableDiffusionInpaintPipeline
+from .config import MODEL_ID, DEVICE, DTYPE
+
+def load_pipeline():
+    pipe = StableDiffusionInpaintPipeline.from_pretrained(
+        MODEL_ID,
+        torch_dtype=DTYPE
+    )
+
+    pipe = pipe.to(DEVICE)
+
+    if DEVICE == "cuda":
+        pipe.enable_xformers_memory_efficient_attention()
+        pipe.enable_model_cpu_offload()
+
+    return pipe
