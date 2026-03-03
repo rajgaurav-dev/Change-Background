9a904462ea3dda1b50c059618e313b8648617ffc
Raj Gaurav <rgt.smarty@gmail.com>
2026-03-03 13:57:04 +0530

    feat:Added dummy script for prompt

---
+++ src/prompts.py
@@ -0,0 +1,10 @@
+BACKGROUND_PROMPTS = [
+    "A muddy farm field during heavy rain",
+    "A dry desert agricultural land",
+    "A snowy farm field in winter",
+    "A green grass pasture during spring",
+    "Golden sunset over farmland",
+    "Industrial warehouse background",
+    "Foggy morning in a large field",
+    "Mountain farming land landscape"
+]
